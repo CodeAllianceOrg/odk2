baf3d21359b6f2b7a26eac0a607f953f8976dcff
Tristan K <russon77@gmail.com>
2017-09-07 10:20:31 -0400

    test: failing e2e

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,19 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
-  }
+    navigateTo() {
+        return browser.get('/');
+    }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
-  }
+    getHeader() {
+        return {
+            getBrandText() {
+                return element(by.css('app-root .header .brand'));
+            }
+        }
+    }
+
+    getParagraphText() {
+        return element(by.css('app-root h1')).getText();
+    }
 }
